5918016e9dc417800e73341a73066cbc1afa45a2
mohamed howedy <howedyy@example.com>
2025-09-26 15:54:29 +0300

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(EventlyApp());
+}
+
+class EventlyApp extends StatelessWidget {
+  const EventlyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: ,
+    );
+  }
+}
